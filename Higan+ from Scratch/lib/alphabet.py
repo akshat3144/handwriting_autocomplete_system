92c8f02eb55eb220611659d83733dbb92325138b
Abhijeet Shashwat <asp616848@gmail.com>
2025-11-26 00:00:15 +0530

    up

--- Higan+ from Scratch/lib/alphabet.py
+++ Higan+ from Scratch/lib/alphabet.py
@@ -97,7 +97,7 @@ class strLabelConverter(object):
             length = torch.IntTensor([nonzero_count(t)])
 
         if length.numel() == 1:
-            length = length[0]
+            length = length.item()
             assert nonzero_count(t) == length, "{} text with length: {} does not match declared length: {}".\
                                                 format(t, nonzero_count(t), length)
             if raw:
